b13eff09ba34b8b7dd5fb3a2f1f2f5a2940c7010
Ylevo <71675020+Ylevo@users.noreply.github.com>
2020-09-22 02:39:32 +0200

    Add files via upload

---
+++ SoundBoard/aboutForm.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SoundBoard
+{
+    public partial class aboutForm : Form
+    {
+        public aboutForm()
+        {
+            InitializeComponent();
+        }
+
+        private void naudioLinkLabel_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            naudioLinkLabel.LinkVisited = true;
+            System.Diagnostics.Process.Start("https://naudio.codeplex.com/license");
+        }
+    }
+}
